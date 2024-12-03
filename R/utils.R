fcccd7203217de81f5d9e13dc83c1c9e839f13e6
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-12-02 16:48:54 -0800

    exported utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,33 @@
-
-#' @noRd
+#' Recode NPI entity codes
+#'
+#' @param x A character vector
+#'
+#' @returns A character vector
+#'
+#' @examples
+#' entity_recode(c("1", "2", "3"))
+#'
+#' @autoglobal
+#'
+#' @export
 entity_recode <- function(x) {
-  data.table::fcase(x == "1", "I", x == "2", "O", default = x)
+  data.table::fcase(x == "1", "I", x == "2", "O", default = NA_character_)
 }
 
-#' @noRd
+#' Make a case statement
+#'
+#' @param x A character vector
+#'
+#' @param y A character vector
+#'
+#' @returns A character vector
+#'
+#' @examples
+#' make_case(c("1", "1", "3", NA), c("1", "2", NA, "4"))
+#'
+#' @autoglobal
+#'
+#' @export
 make_case <- function(x, y) {
   data.table::fcase(
     codex::not_na(x) & codex::not_na(y) & x == y, y,
@@ -14,7 +37,18 @@ make_case <- function(x, y) {
     default = NA_character_)
 }
 
-#' @noRd
+#' Format a 9-digit zip code
+#'
+#' @param x A character vector
+#'
+#' @returns A character vector
+#'
+#' @examples
+#' make_zip(c("31605", "316051234", NA))
+#'
+#' @autoglobal
+#'
+#' @export
 make_zip <- function(x) {
   ifelse(
     codex::not_na(x) & codex::sf_nchar(x) == 9,
@@ -26,37 +60,78 @@ make_zip <- function(x) {
     x)
 }
 
-#' @noRd
+#' Format a 10-digit phone number
+#'
+#' @param x A character vector
+#'
+#' @returns A character vector
+#'
+#' @examples
+#' phone_eq_10(c("9122474701", NA))
+#'
+#' @autoglobal
+#'
+#' @export
 phone_eq_10 <- function(x) {
+
+  x <- x[codex::not_na(x)]
+
   paste0(
     "(",
     substr(x, 1, 3),
-    ")",
+    ") ",
     substr(x, 4, 6),
     "-",
     substr(x, 7, 10)
     )
 }
 
-#' @noRd
+#' Format a 10-digit phone number
+#'
+#' @param x A character vector
+#'
+#' @returns A character vector
+#'
+#' @examples
+#' phone_gt_10(c("9122474701  9122474701", NA))
+#'
+#' @autoglobal
+#'
+#' @export
 phone_gt_10 <- function(x) {
+
+  x <- x[codex::not_na(x)]
+
   paste0(
     "(",
     substr(x, 1, 3),
-    ")",
+    ") ",
     substr(x, 4, 6),
     "-",
     substr(x, 7, 10),
     ", (",
     substr(x, 13, 15),
-    ")-",
+    ") ",
     substr(x, 16, 18),
     "-",
     substr(x, 19, 22)
     )
 }
 
-#' @noRd
+#' Format a 10-digit phone number
+#'
+#' @param x A character vector
+#'
+#' @returns A character vector
+#'
+#' @examples
+#' make_phone("9122474701")
+#'
+#' make_phone("9122474701  9122474701")
+#'
+#' @autoglobal
+#'
+#' @export
 make_phone <- function(x) {
   data.table::fcase(
     codex::not_na(x) & codex::sf_nchar(x) == 10, phone_eq_10(x),
@@ -64,12 +139,51 @@ make_phone <- function(x) {
     default = x)
 }
 
-#' @noRd
+#' Format a Date
+#'
+#' @param x character string
+#'
+#' @param fmt string format, e.g. '%m/%d/%Y'
+#'
+#' @returns date string
+#'
+#' @examples
+#' as_date("12/31/2021")
+#'
+#' @autoglobal
+#'
+#' @export
 as_date <- function(x, fmt = "%m/%d/%Y") as.Date(x, format = fmt)
 
-#' @noRd
+#' Remove periods
+#'
+#' @param x character string
+#'
+#' @returns character string
+#'
+#' @examples
+#' remove_periods("M.D..")
+#'
+#' @autoglobal
+#'
+#' @export
 remove_periods <- function(x) codex::sf_remove(x, "\\.")
 
-#' @noRd
+#' Wrap a string
+#'
+#' @param x character string
+#'
+#' @param left character string
+#'
+#' @param right character string
+#'
+#' @returns character string
+#'
+#' @examples
+#' wrap("912", "(", ")")
+#'
+#' @autoglobal
+#'
+#' @export
 wrap <- function(x, left, right) paste0(left, x, right)
 
